8c1f36a09168a0f16ac8ffc31c4040a63a625d89
Aref Zargar <aref.unlimited.84@gmail.com>
2026-02-14 21:40:23 +0330

    All user methods in the backend were implemented in the user.model ts file.

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,5 +1,32 @@
 export interface User {
   id: string;
+  name: string;
   username: string;
   password: string;
+  age: number;
+  credit: number;
+  email: string;
+}
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
+export interface RegisterRequest {
+  name: string;
+  username: string;
+  password: string;
+  age: number;
+  credit: number;
+  email: string;
+}
+export interface EditUserRequest {
+  Name: string;
+  Username: string;
+  Password: string;
+  Age: number;
+  Email: string;
+}
+export interface AddCreditRequest {
+  Username: string;
+  Amount: number;
 }
