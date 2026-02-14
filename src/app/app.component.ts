a9df699a1844b47448b9250cbb23d3b5ad4064ac
Aref Zargar <aref.unlimited.84@gmail.com>
2026-02-14 21:50:52 +0330

    All component in app.component ts file added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {UserSectionComponent} from "./components/user-section/user-section.component";
+import {ItemSectionComponent} from "./components/item-section/item-section.component";
+import {UserListComponent} from "./components/user-list/user-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserSectionComponent, ItemSectionComponent, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  title = 'MarketplaceSample';
 }
