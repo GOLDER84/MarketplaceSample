28ce6680268d7b4612d84dcf3b03fbc8c866fb7d
Aref Zargar <aref.unlimited.84@gmail.com>
2026-02-14 21:40:47 +0330

    All item methods in the backend were implemented in the item.model ts file.

--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -1,5 +1,15 @@
 export interface Item {
-  id: string;
+  id: number;
   name: string;
   price: number;
+  description: string;
+}
+export interface AddItemRequest {
+  Name: string;
+  Price: number;
+  Description: string;
+}
+export interface BuyItemRequest {
+  Username: string;
+  ItemId: number;
 }
