91fa87f296eb574da312292c13dd3f52a6033c8b
Aref Zargar <aref.unlimited.84@gmail.com>
2026-02-14 21:49:45 +0330

    Added style for the home page.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'IDEProject' title`, () => {
+  it(`should have the 'MarketplaceSample' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('IDEProject');
+    expect(app.title).toEqual('MarketplaceSample');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, IDEProject');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MarketplaceSample');
   });
 });
